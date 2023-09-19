7ea1270f67b8c9ba4188bd6c157e4701b2a86a61
tmurilloa <tmurilloa@unal.edu.co>
2023-09-19 16:22:55 -0500

    update 1

---
+++ zooAnimales/Mamifero.py
@@ -0,0 +1,34 @@
+from Animal import Animal
+class Mamifero(Animal):
+    _listado = []
+    caballos = 0
+    leones = 0
+    def __init__(self,nombre,edad,habitat,genero,pelaje,patas):
+        super().__init__(nombre,edad,habitat,genero)
+        self._pelaje = pelaje
+        self._patas = patas
+        Mamifero._listado.append(self)
+    
+    def cantidadMamiferos(cls):
+        return len(cls._listado)
+    def crearCaballos(cls,nombre,edad,genero):
+        caballo = Mamifero(nombre,edad,"pradera",genero,True,4)
+        cls.caballos += 1
+        return caballo
+    def crearLeones(cls,nombre,edad,genero):
+        leon = Mamifero(nombre,edad,"selva",genero,True,4)
+        cls.leones += 1
+        return leon
+    
+    def getPelaje(self):
+        return self._pelaje
+    def setPelaje(self,pelaje):
+        self._pelaje = pelaje
+    def getPatas(self):
+        return self._patas
+    def setPatas(self,patas):
+        self._patas = patas
+    def getListado(cls):
+        return cls._listado
+    
+
---
+++ zooAnimales/Pez.py
@@ -0,0 +1,34 @@
+from Animal import Animal
+class Pez(Animal):
+    _listado = []
+    salmones = 0
+    bacalaos = 0
+    def __init__(self,nombre,edad,habitat,genero,colorEscamas,cantidadAletas):
+        super().__init__(nombre,edad,habitat,genero)
+        self._colorEscamas = colorEscamas
+        self._cantidadAletas = cantidadAletas
+        Pez._listado.append(self)
+    
+    def cantidadPeces(cls):
+        return len(cls._listado)
+    def movimiento(self):
+        return "nadar"
+    def crearSalmon(cls,nombre,edad,genero):
+        salmon = Pez(nombre,edad,"oceano",genero,"rojo",6)
+        cls.salmones += 1
+        return salmon
+    def crearBacalao(cls,nombre,edad,genero):
+        bacalao = Pez(nombre,edad,"oceano",genero,"gris",6)
+        cls.bacalaos += 1
+        return bacalao
+
+    def setColorEscamas(self,colorEscamas):
+        self._colorEscamas = colorEscamas
+    def getColorEscamas(self):
+        return self._colorEscamas
+    def setCantidadAletas(self,cantidadAletas):
+        self._cantidadAletas = cantidadAletas
+    def getCantidadAletas(self):
+        return self._cantidadAletas
+    def getListado(cls):
+        return cls._listado
